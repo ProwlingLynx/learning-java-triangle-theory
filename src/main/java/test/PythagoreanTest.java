df486eee9a5fbd5cced9194ccb1f0a228433be66
ProwlingLynx <tamezjavier@gmail.com>
2022-11-24 01:02:11 -0600

    adding expectation in comment.

--- src/main/java/test/PythagoreanTest.java
+++ src/main/java/test/PythagoreanTest.java
@@ -5,6 +5,7 @@ import fun.java.Pythagorean;
 public class PythagoreanTest {
     public static void main(String[] args) {
         Pythagorean pythagorean = new Pythagorean(3, 4);
+//        Given legs 3 and 4. We should expect a value of 5 back.
         pythagorean.calculateHypotenuse();
         System.out.println(pythagorean.getHypotenuse());
     }
